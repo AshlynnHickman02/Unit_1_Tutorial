1204c2a89e9903502c062e9fc1bb7d300bf65b49
Ashlynn <ahic486@west-mec.org>
2020-01-08 13:04:18 -0700

    Notes Day 1

---
+++ tny_script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: 
+   Date:   
+
+*/
+console.log("Hello World! Using console.log makes this text show up in the console log in the inspect element.");
+console.log("3+4");
+console.log(4+"ex");
+
+window.alert("Welcome to Tulsa");
+ 
+Window.Alert("you a poopie");
+
+document.getElementById("dateNow");
+//class  method          attribute 
+//object1                parameters
+
+document.getSelection.name("Jeff");
+//class               method
+//object1                   parameters
