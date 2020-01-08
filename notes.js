99a8c94d4ce9a5f5324d3e914ee61a820b0be0d4
Ashlynn <ahic486@west-mec.org>
2020-01-08 15:26:35 -0700

    Notes Day 3
    Absent for Day 2

---
+++ notes.js
@@ -0,0 +1,48 @@
+console.log("Hello World! Using console.log makes this text show up in the console log in the inspect element.");
+console.log("3+4");
+console.log(4+"ex");
+
+window.alert("Welcome to Tulsa");
+ 
+Window.Alert("you a poopie");
+
+document.getElementById("dateNow");
+//class  method          attribute 
+//object1                parameters
+
+document.getSelection.name("Jeff");
+//class               method
+//object1                   parameters
+ 
+
+//creating variables
+         var //variable
+
+         var x=7; //numeric value/variable
+
+         var str = "string"; //string variable
+
+         var peterParker = new Student('very', "slightly", .894); //object variable
+         
+         var garfield = new Cat("lazy", "fat", "hates Mondays");
+
+         var r = 3; 
+         var circumference = 2*Math.PI*r; //2 times Pi times radius 
+         console.log(circumference); //go to inspect to find this answer 
+//
+
+6 + 5 = x; //variable first
+x = 6 + 5; 
+
+var x = 7; 
+x += 3; //x = x + 3 == x = 7 + 3 ==             x = 10
+const GRAVITY_EARTH = 9.8; //constants are CAPITALIZED and THEY_ARE_UNDERSCORED
+const GRAVITY_MOON = 3.2;
+
+//return function 
+var squareArea = calcArea(6, 6); 
+function calcArea(length, width){
+   var area = length*width;
+   return area; 
+}
+console.log(squareArea);
